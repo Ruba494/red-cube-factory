ce8b2993933a45b77a112a20c215db630c922243
r.almutairi <r.almutairi@estishraf.com>
2025-04-13 13:51:29 +0300

    feat: WIP loading

--- src/components/loading/loadingPage.tsx
+++ src/components/loading/loadingPage.tsx
@@ -1,7 +1,23 @@
 export const LoadingPage = () => {
     return<div className={'loading-page'}>
         loading
-
+        <div className="scene">
+            <div className="cube-wrapper">
+                <a href="">
+                    <div className="cube">
+                        <div className="cube-faces">
+                            <div className="cube-face shadow"></div>
+                            <div className="cube-face bottom"></div>
+                            <div className="cube-face top"></div>
+                            <div className="cube-face left text">1</div>
+                            <div className="cube-face right text">2</div>
+                            <div className="cube-face front text">3</div>
+                            <div className="cube-face back"></div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
 
     </div>
 }
