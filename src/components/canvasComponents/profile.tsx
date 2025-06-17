f870f449ae19cca5cf23eb96522815904a58f74b
r.almutairi <r.almutairi@estishraf.com>
2025-06-17 15:14:14 +0300

    feat: add info tags on profile

--- src/components/canvasComponents/profile.tsx
+++ src/components/canvasComponents/profile.tsx
@@ -1,12 +1,39 @@
 import {forwardRef, useEffect, useImperativeHandle, useRef} from "react";
 import gsap from 'gsap';
 import {PixelatedComponent} from "../PixelatedComponent.tsx";
+import {Tags} from "../Tags.tsx";
+import {Colors} from "../../pages/constants/colors.ts";
 
 const DURATION=[0,0.3,0.7,1]
 
 export const Profile = () => {
+    const ContactsTags=[
+        {tag:'diaries account', color:Colors.RED,link:'https://www.instagram.com/ruu.diary'},
+        {tag:'professional ruba', color:Colors.RED,link:'https://sa.linkedin.com/in/ruba-almutairi-2000/en'},
+    ]
+    const ExtraInfoTags=[
+        {tag:'25', color:Colors.GREEN,},
+        {tag:'INFJ', color:Colors.BLUE,},
+        {tag:'ONEDOOR', color:Colors.PINK,},
+        {tag:'احب اللون الاحمر', color:Colors.RED, lang:'ar'},
+        {tag:'日本語', color:Colors.PURPLE, lang:'jp'},
+        {tag:'English', color:Colors.GRAY},
+        {tag:'عربي', color:Colors.YELLOW,lang:'ar'},
+    ]
+
     return <div className={'profile'}>
         <span className={'title'}> Hi, I'm  <span className={'title-highlight'}>Ruba!</span></span>
+        <div className={'extra-info'}>
+            <ul className={'tags'}>
+                {
+                    ExtraInfoTags.map((tag, index) => (
+                        <li className={`tags-item ${tag.color} `} lang={tag?.lang??'en'} key={index}>
+                            {tag.tag}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
         <span className={'subtitle'}> I'm a software engineer who loves writing digital diaries.</span>
         <span className={'text'}>
             I created this website to share my own diary entries and the templates I design. My goal is to inspire others to start documenting their thoughts, memories, and everyday moments.
@@ -15,21 +42,24 @@ export const Profile = () => {
              <span className={'ending-hearts'}> </span>
             Feel free to explore and use anything you like—I’d love to see how you make it your own!
         </span>
-        <div className={'doodles'}>
-            <span className={'doodles-cube'}> </span>
-            <span className={'doodles-flower'}> </span>
-            <span className={'doodles-arrow'}> </span>
-            <span className={'doodles-stars'}> </span>
-            <span className={'doodles-home'}> </span>
-        </div>
+        {/*<div className={'doodles'}>*/}
+        {/*    <span className={'doodles-cube'}> </span>*/}
+        {/*    <span className={'doodles-flower'}> </span>*/}
+        {/*    <span className={'doodles-arrow'}> </span>*/}
+        {/*    <span className={'doodles-stars'}> </span>*/}
+        {/*    <span className={'doodles-home'}> </span>*/}
+        {/*</div>*/}
 
         <div className={'contacts'}>
             <span className={'text'}>Whether it's a question, an idea, or just a hello—feel free to reach out on </span>
-            <ul>
-                <li>
-                    <a className={'link'} href={'https://www.instagram.com/ruu.diary'}>ruu.diary</a>
-                </li>
-                <li> <a className={'link'} href={'https://sa.linkedin.com/in/ruba-almutairi-2000/en'}>Ruba Almutairi</a></li>
+            <ul className={'tags'}>
+                {
+                    ContactsTags.map((tag, index) => (
+                        <li className={`tags-item ${tag.color} `} lang={tag?.lang??'en'} key={index}>
+                            <a className={'link'} href={tag.link}>{tag.tag}</a>
+                        </li>
+                    ))
+                }
             </ul>
         </div>
     </div>
