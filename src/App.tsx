d67d6ce2723d0ad4ffb965c005e76ef2f37d30cf
r.almutairi <r.almutairi@estishraf.com>
2025-07-10 21:00:00 +0300

    feat: update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import {Navigation} from "./components/navigation.tsx";
-import {BrowserRouter, Outlet, useLocation,} from "react-router";
+import { Outlet, useLocation,} from "react-router";
 import {PageRoutes, PATHS_CONSTANTS, PATHS_CONSTANTS_ENUM} from "./routes";
 import {Cursor} from "./components/cursor.tsx";
 import {GoToTemplates} from "./components/templatesComponents/goToTemplates.tsx";
 function App() {
     const location = useLocation();
     const isTemplatePage = location.pathname === PATHS_CONSTANTS[PATHS_CONSTANTS_ENUM.TEMPLATES];
-    return <BrowserRouter basename="/red-cube-factory">
-        <div className={'app'}  >
+    return<div className={'app'}  >
             <Cursor/>
             {!isTemplatePage&&<GoToTemplates/>}
             <Navigation/>
             <PageRoutes/>
             <Outlet />
         </div>
-    </BrowserRouter>
 
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './assets/styles/main.scss'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router";
+const isProd = import.meta.env.MODE === 'production'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+    <BrowserRouter basename={isProd ? '/red-cube-factory' : '/'}>
+        <App />
+    </BrowserRouter>,
 )
