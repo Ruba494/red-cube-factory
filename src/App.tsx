7b4e82f16c0b5e83a2f547007faa95c4408c674b
r.almutairi <r.almutairi@estishraf.com>
2025-07-10 20:46:35 +0300

    feat: update config

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import {Navigation} from "./components/navigation.tsx";
-import {Outlet, useLocation,} from "react-router";
+import {BrowserRouter, Outlet, useLocation,} from "react-router";
 import {PageRoutes, PATHS_CONSTANTS, PATHS_CONSTANTS_ENUM} from "./routes";
 import {Cursor} from "./components/cursor.tsx";
 import {GoToTemplates} from "./components/templatesComponents/goToTemplates.tsx";
 function App() {
     const location = useLocation();
     const isTemplatePage = location.pathname === PATHS_CONSTANTS[PATHS_CONSTANTS_ENUM.TEMPLATES];
-    return <div className={'app'}  >
-        <Cursor/>
-        {!isTemplatePage&&<GoToTemplates/>}
-        <Navigation/>
-        <PageRoutes/>
-        <Outlet />
-    </div>
+    return <BrowserRouter basename="/red-cube-factory/">
+        <div className={'app'}  >
+            <Cursor/>
+            {!isTemplatePage&&<GoToTemplates/>}
+            <Navigation/>
+            <PageRoutes/>
+            <Outlet />
+        </div>
+    </BrowserRouter>
+
 }
 
 export default App
