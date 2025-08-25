ce2c5d10e3871cd0917877567ef5b8fbaded84ef
r.almutairi <r.almutairi@estishraf.com>
2025-08-25 12:32:20 +0300

    chore: handel image sources

--- src/utils/getAsset.ts
+++ src/utils/getAsset.ts
@@ -1,4 +1,4 @@
 export function getAsset(path: string): string {
     // Works both in dev and production
-    return new URL(path, import.meta.url).href;
+    return import.meta.env.BASE_URL+path;
 }
